a0f09ee23dfb2a3737d88c3c01e7c495e386ca72
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-04-01 19:49:24 +0900

    Initial Commit

---
+++ src/test/java/go/seoul/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package go.seoul.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
